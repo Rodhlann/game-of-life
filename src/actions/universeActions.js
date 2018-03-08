05f330aab226f46342ef7484d0f41a6407b9148e
Rodhlann <tjpepper93@gmail.com>
2018-03-07 23:49:51 -0500

    comment the code

--- src/actions/universeActions.js
+++ src/actions/universeActions.js
@@ -1,33 +1,31 @@
 import * as actionTypes from './actionTypes';
 
-export function deployUniverse(width, height) {
-  return { type: actionTypes.DEPLOY_UNIVERSE, width, height };
-}
-
-export function discoverUniverse(width, height) {
-  return (dispatch) => {
-    dispatch(deployUniverse(width, height));
-  };
-}
-
-export function createUniverseCell(cells) {
-  return (dispatch) => {
-    dispatch({ type: actionTypes.CREATE_UNIVERSE_CELL_DATA, cells });
-  };
-}
-
+/**
+  * Toggle universe on/off
+  * @param {bool} toggle represents the new state of the universe
+  */
 export function toggleActive(toggle) {
   return (dispatch) => {
     dispatch({ type: actionTypes.TOGGLE_ACTIVE, universeActive: toggle });
   };
 }
 
+/**
+  * Batch update all statuses
+  * @param {array} universeCellStatuses 2D array representing the state of all cells in the universe
+  */
 export function updateAllStatuses(universeCellStatuses) {
   return (dispatch) => {
     dispatch({ type: actionTypes.UPDATE_ALL_STATUSES, universeCellStatuses });
   };
 }
 
+/**
+  * Toggle the state of a cell
+  * @param {number} rowIndex y index of the universe
+  * @param {number} cellIndex x index of the universe
+  * @param {array} universeCellStatuses 2D array representing the state of all cells in the universe
+  */
 export function toggleStatus(rowIndex, cellIndex, universeCellStatuses) {
   const tmpArray = universeCellStatuses;
   tmpArray[rowIndex][cellIndex] = !universeCellStatuses[rowIndex][cellIndex];
@@ -36,6 +34,11 @@ export function toggleStatus(rowIndex, cellIndex, universeCellStatuses) {
   };
 }
 
+/**
+  * Updates the width of the universe
+  * @param {number} width represents new user input
+  * @param {number} height  represents the current height of the universe
+  */
 export function updateUniverseWidth(width, height) {
   return (dispatch) => {
     dispatch({
@@ -46,6 +49,11 @@ export function updateUniverseWidth(width, height) {
   };
 }
 
+/**
+  * Updates the height of the universe
+  * @param {number} width represents the current width of the universe
+  * @param {number} height  represents new user input
+  */
 export function updateUniverseHeight(width, height) {
   return (dispatch) => {
     dispatch({
@@ -56,6 +64,11 @@ export function updateUniverseHeight(width, height) {
   };
 }
 
+/**
+  * Generate initial 2D array representation of the universe
+  * @param {number} width the width of the universe
+  * @param {number} height the height of the universe
+  */
 export function addUniverseCellStatuses(width, height) {
   return (dispatch) => {
     dispatch({
